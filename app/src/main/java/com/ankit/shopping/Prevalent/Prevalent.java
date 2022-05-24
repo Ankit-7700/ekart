8ff6e7ecbfcbae6c439a38456f85b2a1e99fbe0b
Ankit-7700 <ankit.pal_cs19@gla.ac.in>
2022-05-24 12:47:33 +0530

    project ekart commit

---
+++ app/src/main/java/com/ankit/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.ankit.shopping.Prevalent;
+
+import com.ankit.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
